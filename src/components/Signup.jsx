c9bf3ce210cf70cad7b2ba562832677ef2bb0f0a
Gulshan-00 <rr3870044@gmail.com>
2025-10-25 14:09:15 +0530

    added the API in signup route

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,13 +1,48 @@
-import React from 'react'
-import { Link } from 'react-router';
+import axios from 'axios';
+import React, { useState } from 'react'
+import { Link, Navigate } from 'react-router';
+import { BASE_URL } from '../utils/Constents';
 
 const Signup = () => {
+  const [firstName, setFirstName] = useState("");
+  const [emailId, setEmailId] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post(
+        BASE_URL +"/signup",
+        {
+          firstName,
+          emailId,
+          password,
+        },
+        {
+          withCredentials: true,
+        }
+      );
+        console.log(response.status())
+
+       //the data which is coming from the backend
+      //  const data= await response.data;
+      //  Dispatch(addUser(data));
+
+       Navigate('/login');
+
+    } catch (error) {
+      console.error("ERROR:"+ error.message);
+    }
+  };
+
+
   return (
     <div className="bg-blue-400 shadow-2xl flex flex-col justify-center self-center p-20 rounded-xl ">
 
           <label className="label font-bold text-black">First Name</label>
           <input
             type="name"
+            value={firstName}
+            onChange={(e)=>{setFirstName(e.target.value)}}
             className="input px-10"
             placeholder="First Name"
           />
@@ -15,12 +50,16 @@ const Signup = () => {
           <label className="label font-bold text-black">Email</label>
           <input
             type="email"
+            value={emailId}
+            onChange={(e)=>{setEmailId(e.target.value)}}
             className="input"
             placeholder="Email"
           />
           <label className="label font-bold text-black">Password</label>
           <input
             type="password"
+            value={password}
+            onChange={(e)=>{setPassword(e.target.value)}}
             className="input"
 
             placeholder="Password"
@@ -28,7 +67,7 @@ const Signup = () => {
           <div className="self-center">
             <a className="link link-hover font-bold text-black">Forgot password?</a>
           </div>
-          <button className="btn btn-neutral mt-4 hover:bg-blue-400 hover:text-blue-950">
+          <button onClick={handleLogin} className="btn btn-neutral mt-4 hover:bg-blue-400 hover:text-blue-950">
             SignUp
           </button>
 
